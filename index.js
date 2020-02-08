2f81292e970aef57843b174ac880a806db12c6bf
Khoa <52121010+11Khoa@users.noreply.github.com>
2020-02-08 11:10:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express=require("express");
 
 const TelegramBot = require("node-telegram-bot-api");
 
-const TOKEN =process.env.TELEGRAM_TOKEN || "1023567659:AAEGEBYyns1m3Lvcq98aR3wi-Z8Bn3FWP8A",
+const TOKEN =process.env.TELEGRAM_TOKEN || "1023567659:AAEGEBYyns1m3Lvcq98aR3wi-Z8Bn3FWP8A";
 
 const options = {
     webHook: {
